28249e782a3457ef40545ffe8dc04038d16a8777
zzxsadf <2516511081@qq.com>
2025-01-18 15:40:14 +0800

    1.js

---
+++ Javascript/1.js
@@ -0,0 +1,19 @@
+// let a = 3;
+// let b = 5;
+// let temp=a;
+// a=b;
+// b=temp;
+// console.log(a,b);
+
+// let a = 3;
+// let b = 5;
+// a+=b;
+// b=a-b;
+// a-=b;
+// console.log(a,b)
+
+
+let a = 3;
+let b = 5;
+[a, b] = [b, a];
+console.log( a, b);  
